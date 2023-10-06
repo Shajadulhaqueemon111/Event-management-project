daf31f9b37de29a7f3011cd080a7c8a3e23207d8
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-10-06 19:30:02 +0600

    first commit

---
+++ src/Components/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBX2mNqKND87QkYhgcVzoXg92NJz1pCeYk",
+  authDomain: "event-management-auth-1a138.firebaseapp.com",
+  projectId: "event-management-auth-1a138",
+  storageBucket: "event-management-auth-1a138.appspot.com",
+  messagingSenderId: "314043649311",
+  appId: "1:314043649311:web:5f39d2997b3ef6eb03342a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
